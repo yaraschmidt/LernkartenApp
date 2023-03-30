7cf9f97be28e0ea16f3b54aa7cdf2a044e1677ed
Niklas Haas <niklas.haas@sap.com>
2023-03-30 11:32:12 +0200

    Init

---
+++ backend-2/index.js
@@ -0,0 +1,184 @@
+import express from "express";
+import cors from "cors";
+import { connect } from "./db.js";
+import { ObjectId } from "mongodb";
+
+const [userCollection, cardCollection] = await connect();
+const server = express();
+const baseUrl = "http://localhost:3000";
+
+function processUser(user){
+    if(!user) return user;
+    user.links = {
+        create   : { url : baseUrl, method : "POST" },
+        getAll   : { url : baseUrl, method : "GET" },
+        get      : { url : baseUrl + "/" + user._id,                   method : "GET" },
+        update   : { url : baseUrl + "/" + user._id,                   method : "PUT" },
+        delete   : { url : baseUrl + "/" + user._id,                   method : "DELETE" },
+        nextCard : { url : baseUrl + "/cards/nextForUser/" + user._id, method : "GET" },
+    }
+    return user;
+}
+
+function processCard(card){
+    console.log(card);
+    if(!card)
+        return { links :{
+                create : { url : baseUrl, method : "POST" },
+                getAll : { url : baseUrl, method : "GET" }
+            }
+        }
+    card.links = {
+        create : { url : baseUrl,                   method : "POST" },
+        getAll : { url : baseUrl,                   method : "GET" },
+        solve  : { url : baseUrl + "/solve",        method : "POST" },
+        get    : { url : baseUrl + "/" + card._id,  method : "GET" },
+        update : { url : baseUrl + "/" + card._id,  method : "PUT" },
+        delete : { url : baseUrl + "/" + card._id,  method : "DELETE" },
+    }
+    return card
+}
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/user", async (req, res) => {
+    userCollection.find({}).toArray()
+        .then(userArr => userArr.map(processUser))
+        .then(user => res.json(user));
+});
+
+server.get("/user/:id", async (req, res) => {
+    const {id,} = req.params;
+    if(!id) return res.sendStatus(400);
+
+    userCollection.findOne({_id : new ObjectId(id)})
+        .then(processUser)
+        .then(user => res.json(user))
+});
+
+server.post("/user", async (req, res) => {
+    const {name, age, color} = req.body;
+    if(!name || !age || !color) return res.sendStatus(400);
+    const user = {
+        name : name,
+        age : age,
+        color : color
+    }
+    userCollection.insertOne(user)
+        .then(val => {
+            user._id = val.insertedId;
+            return user;
+        })
+        .then(processUser)
+        .then(user => res.json(user));
+});
+
+server.put("/user", async (req, res) => {
+    const {_id, name, age, color} = req.body;
+    if(!_id || !name || !age || !color) return res.sendStatus(400);
+    const user = {
+        _id : new ObjectId(_id),
+        name : name,
+        age : age,
+        color : color
+    }
+    userCollection.updateOne({_id :  user._id}, {$set : user})
+        .then(() => user)
+        .then(processUser)
+        .then(user => res.json(user));
+});
+
+server.delete("/user/:id", (req, res) => {
+    const {id,} = req.params;
+    if(!id) return res.sendStatus(400);
+    userCollection.deleteOne({_id : new ObjectId(id)})
+        .then(resp => (resp.acknowledged && resp.deletedCount == 1) ? 200 : 404 )
+        .then(code => res.sendStatus(code));
+});
+
+server.get("/cards", (req, res) => {
+    cardCollection.find({}).toArray()
+        .then(arr => arr.map(processCard))
+        .then(cards => res.json(cards));
+});
+
+server.get("/cards/:id", (req, res) => {
+    const {id, } = req.params;
+    if(id == undefined) return res.sendStatus(400);
+    cardCollection.findOne({_id : new ObjectId(id)})
+        .then(processCard)
+        .then(card => res.json(card));
+})
+
+server.post("/cards", (req, res) => {
+    const {question, answer, color} = req.body;
+    if( question == undefined || answer == undefined || color == undefined) return res.sendStatus(404);
+    const card = {
+        question : question,
+        answer : answer,
+        color : color,
+        user : []
+    }
+    cardCollection.insertOne(card)
+        .then(resp => resp.insertedId)
+        .then(id => {card._id = id; return card;})
+        .then(processCard)
+        .then(card => res.json(card));
+});
+
+server.post("/cards/solve", async (req, res) => {
+    const {cardId, userId, answerKind} = req.body;
+    console.log(cardId, userId);
+    if(cardId == undefined || userId == undefined) return res.sendStatus(400);
+    userCollection.findOne({_id : new ObjectId(userId)})
+        .then(user => (!user) ? Promise.reject("") : user )
+        .then(user => 
+            cardCollection.updateOne({_id : new ObjectId(cardId)}, {$push : {"user" : { userId : user._id, kind : answerKind }}})
+        )
+        .then(() => res.sendStatus(200))
+        .catch(() => res.sendStatus(403))
+});
+
+server.get("/cards/nextForUser/:id", async (req, res) => {
+    const { id } = req.params;
+    if(id == undefined ) return res.sendStatus(400);
+    cardCollection.findOne({
+        $or : [
+            { user : { $all : [ { "$elemMatch" : { userId : { $ne : new ObjectId(id) } } } ]} },
+            { user : { $eq : [] } }
+        ]
+    })
+        .then(val => console.log(val))
+        .then(processCard)
+        .then(card => res.json(card));
+});
+
+server.put("/cards", (req, res) => {
+    const { _id, question, answer, color } = req.body;
+    if(_id == undefined || question == undefined || answer == undefined || color == undefined ) return res.sendStatus(400);
+    const card = {
+        _id : new ObjectId(_id),
+        question : question,
+        answer : answer,
+        color : color
+    }
+    cardCollection.updateOne({_id : card._id}, {$set : card})
+        .then(resp => (resp.acknowledged && resp.modifiedCount == 1) ? card : null)
+        .then(processCard)
+        .then(card => res.json(card))
+});
+
+server.delete("/cards/:id", (req, res) => {
+    const { id } = req.params;
+    if(id == undefined) return res.sendStatus(400);
+    cardCollection.deleteOne({_id : new ObjectId(id)})
+        .then(resp => (resp.acknowledged && resp.deletedCount == 1) ? 200 : 403)
+        .then(code => res.sendStatus(code));
+});
+
+
+
+server.listen(3000, "0.0.0.0", () => {
+    console.log("Server gestartet!");
+});
---
+++ frontend/index.js
@@ -0,0 +1,222 @@
+let HTML_TEMPLATE = "";
+
+const backendAPI = "http://localhost:3000";
+
+function deleteUser(userId){
+    fetch(backendAPI + "/user/" + userId,{
+        method : "DELETE"
+    })
+    .then(() => route('users'));
+}
+
+function route (target) {
+    if(window.location.hash === "#" + target) return window.location.reload();
+    window.location.href = "./#" + target;
+    // window.location.reload(true);
+}
+
+function showBack(){
+    const backSide = document.getElementsByClassName("back")[0];
+    backSide.style.display = "flex";
+}
+
+window.addEventListener("hashchange", func);
+window.addEventListener("load", func);
+
+async function func(){
+    const pageSplit = location.hash.slice(1).split("/");
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "";
+    
+    console.log(location.hash);
+    console.log(pageSplit);
+    switch (pageSplit[0]) {
+        case "users":
+            await fetch("./templates/users.html")
+                .then(resp => resp.text())
+                .then(text => {HTML_TEMPLATE = text});
+
+
+            const userListDiv = document.createElement("div");
+            userListDiv.classList.add("list")
+
+            fetch(backendAPI + "/user")
+                .then(user => user.json())
+                .then(arr => arr.forEach(user => {
+                    let html = HTML_TEMPLATE;
+
+                    html = html.replace(/%NAME%/g, user["name"]);
+                    html = html.replace(/%AGE%/g, user["age"]);
+                    html = html.replace(/%COLOR%/g, user["color"]);
+                    html = html.replace(/%USERID%/g, user["_id"]);
+                    userListDiv.innerHTML += html;
+                }));
+
+                const createUserButton = document.createElement("button");
+                createUserButton.onclick = () => route('user');
+                createUserButton.innerText = "Create User"
+                contentDiv.appendChild(userListDiv);
+                contentDiv.appendChild(createUserButton);
+
+            break;
+    
+        case "user":
+            await fetch("./templates/user.html")
+                .then(resp => resp.text())
+                .then(text => {HTML_TEMPLATE = text});
+            
+            const userForm = document.createElement("form");
+            
+            var html     = HTML_TEMPLATE;
+            var isUpdate = pageSplit.length > 1;
+            var user = {
+                "name" : "",
+                "age" : 0,
+                "color" : "#ffffff"
+            }
+            if(isUpdate){
+                await fetch(backendAPI + "/user/" + pageSplit[1])
+                .then(resp => (resp.ok) ? resp.json() : user )
+                .then(u => {user = u});
+            }
+            
+            html = html.replace(/%NAME%/g, user.name);
+            html = html.replace(/%AGE%/g, user.age);
+            html = html.replace(/%COLOR%/g, user.color);
+            html = html.replace(/%USER_ACTION%/g, (isUpdate) ? 'Update User' : 'Create User');
+            
+            userForm.innerHTML = html;
+            userForm.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                const body = {
+                    _id : pageSplit[1],
+                    name : e.target.name.value,
+                    age : e.target.age.value,
+                    color : e.target.color.value
+                };
+                console.log(body);
+                await fetch(backendAPI + "/user/", {
+                    method : (isUpdate) ? 'PUT' : 'POST',
+                    headers : {
+                        'Content-Type': 'application/json'
+                    },
+                    body : JSON.stringify(body)
+                })
+                
+                route('users');
+            });
+            contentDiv.appendChild(userForm);
+            document.getElementById("deleteUser").addEventListener('click', () => {
+                deleteUser(user._id);
+            })
+
+            break;
+            
+            case "cards":
+                await fetch("./templates/cards.html")
+                    .then(resp => resp.text())
+                    .then(text => {HTML_TEMPLATE = text});
+
+
+                const cardListDiv = document.createElement("div");
+                cardListDiv.classList.add("list")
+
+                fetch(backendAPI + "/cards")
+                    .then(cards => cards.json())
+                    .then(arr => arr.forEach(card => {
+                        let html = HTML_TEMPLATE;
+
+                        html = html.replace(/%QUESTION%/g, card["question"]);
+                        html = html.replace(/%ANSWER%/g, card["answer"]);
+                        html = html.replace(/%COLOR%/g, card["color"]);
+                        html = html.replace(/%CARDID%/g, card["_id"]);
+                        cardListDiv.innerHTML += html;
+                    }));
+
+                    const createCardButton = document.createElement("button");
+                    createCardButton.innerHTML = "Create Card";
+                    createCardButton.onclick = () => route('card');
+                    contentDiv.appendChild(cardListDiv);
+                    contentDiv.appendChild(createCardButton);
+
+                break;
+
+        case "card":
+            await fetch("./templates/card.html")
+            .then(resp => resp.text())
+            .then(text => {HTML_TEMPLATE = text});
+        
+            const cardForm = document.createElement("form");
+            
+            var html     = HTML_TEMPLATE;
+            var isUpdate = pageSplit.length > 1;
+            var card = {
+                "question" : "",
+                "answer" : "",
+                "color" : "#ffffff"
+            }
+            if(isUpdate){
+                await fetch(backendAPI + "/cards/" + pageSplit[1])
+                .then(resp => (resp.ok) ? resp.json() : card )
+                .then(c => {card = c});
+            }
+            
+            html = html.replace(/%QUESTION%/g, card.question);
+            html = html.replace(/%ANSWER%/g, card.answer);
+            html = html.replace(/%COLOR%/g, card.color);
+            html = html.replace(/%CARD_ACTION%/g, (isUpdate) ? 'Update Card' : 'Create Card');
+            
+            cardForm.innerHTML = html;
+            cardForm.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                const body = {
+                    _id : pageSplit[1],
+                    question : e.target.question.value,
+                    answer : e.target.answer.value,
+                    color : e.target.color.value
+                };
+                console.log(body);
+                await fetch(backendAPI + "/cards/", {
+                    method : (isUpdate) ? 'PUT' : 'POST',
+                    headers : {
+                        'Content-Type': 'application/json'
+                    },
+                    body : JSON.stringify(body)
+                })
+                
+                route('cards');
+            });
+            contentDiv.appendChild(cardForm);
+            break;
+
+        case "learn":
+            if(pageSplit[1]){
+                let user;
+                await fetch(backendAPI + "/user/" + pageSplit[1])
+                    .then(resp => resp.json())
+                    .then(u => {user = u});
+                    
+                if(user){
+                    let card;
+                    await fetch(user.links.nextCard.url)
+                        .then(resp => resp.json())
+                        .then(c => {card = c});
+                    if(card._id){
+                        await fetch("./templates/learn.html")
+                            .then(resp => resp.text())
+                            .then(text => {HTML_TEMPLATE = text});
+                        
+                        HTML_TEMPLATE = HTML_TEMPLATE.replace(/%QUESTION%/g, card.question);
+                        HTML_TEMPLATE = HTML_TEMPLATE.replace(/%ANSWER%/g, card.answer);
+                        HTML_TEMPLATE = HTML_TEMPLATE.replace(/%COLOR%/g, card.color);
+    
+                        contentDiv.innerHTML = HTML_TEMPLATE;
+                        break;
+                    }
+                }
+            }
+
+        default:
+            break;
+    }
+};
